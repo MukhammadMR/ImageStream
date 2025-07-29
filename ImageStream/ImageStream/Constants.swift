37c0b093f93c1876c3b0917b38a9a0c033b753d0
Mukhammad <m.makhmudoff@gmail.com>
2025-07-29 17:12:17 +0300

    Рефакторинг: перенос логики WebView в презентер и AuthHelper

--- ImageStream/ImageStream/Constants.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-enum Constants {
-    static let accessKey = "VKPukYIfL9RnIEHazAuOXyUG-phr-joygIHzml9otGc"
-    static let secretKey = "hloHo6PhLiF8DUJc8TTxzlAIee5o9TB2tEv8yhNEnAc"
-    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
-}
